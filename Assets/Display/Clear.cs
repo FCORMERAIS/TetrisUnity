1ae61518a2ecf04ec26acca45f5dbcb12990211f
JOUTIN <jules.outin@ynov.com>
2022-09-27 16:27:12 +0200

    VerifylineCompleted

--- Assets/Display/Clear.cs
+++ Assets/Display/Clear.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 public class Clear {
-    public static void clear(){
+    public static void clearGrid(){
         List<List<SquareColor>> board = new List<List<SquareColor>>(); 
         // si une forme touche le haut
         for (int i = 0; i < 22; i++){
@@ -16,33 +16,11 @@ public class Clear {
             Game.MirrorGrid[i] = new List<SquareColor>(Ligne);
             Game.ShowTetris[i] = new List<SquareColor>(Ligne);
         }
-        
     }
 
-    // Si une ligne est complète
+    // Si une ligne est complète on la supprime et on descend les lignes au dessus
     public static void ClearLine(){
-        for (int i = 0; i < 22; i++){
-            int count = 0;
-            for (int j = 0; j < 10; j++){
-                if (Game.Grid[i][j] != SquareColor.TRANSPARENT){
-                    count++;
-                }
-            }
-            if (count == 10){
-                for (int k = i; k < 21; k++){
-                    Game.Grid[k] = Game.Grid[k+1];
-                }
-                List<SquareColor> Ligne = new List<SquareColor>();
-                for (int j = 0; j < 10; j++){
-                    SquareColor color = SquareColor.TRANSPARENT;
-                    Ligne.Add(color);
-                }
-                Game.Grid[21] = Ligne;
-                Game.MirrorGrid[21] = new List<SquareColor>(Ligne);
-                Game.ShowTetris[21] = new List<SquareColor>(Ligne);
-                i--;
-            }
-        }
+       
     }
 
     // Vérifie si une ligne est complète
@@ -54,13 +32,7 @@ public class Clear {
                     count++;
                 }
             }
-            if (count == 10){
-                Game.score += 100;
-            }
+           
         }
-    }
-
-    public static void ShowScore(){
-        
     }
 }

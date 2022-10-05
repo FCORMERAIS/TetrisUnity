eea622a35f07409237214474800be16e34c53e0b
Flavio <flavio.cormerais@ynov.com>
2022-10-05 15:51:06 +0200

    version final + commentaires

--- Assets/Display/Board.cs
+++ Assets/Display/Board.cs
@@ -108,9 +108,9 @@ public class Board{
         Game.xPiece = 5;
         Game.yPiece = 1;
     }
-    // Cette fonction permet de vérifier si la pièce peut être placée sur les pièces déjà présentes
+    // Cette fonction permet de vérifier si la pièce peut être placée
     // Elle retourne un booléen
-    public static bool isFloorTouch(){
+    public static bool IsFloorTouch(){
         int x;
         int y;
         for (int i = 0;i<Game.MirrorGrid.Count;i++){
@@ -119,12 +119,12 @@ public class Board{
                     x=j;
                     y=i;    
                     if(y==21 ||Game.Grid[y+1][x] != SquareColor.TRANSPARENT ){
-                        Game.score += 10;
-                        if (1-(Game.score/5000.0f) < 0.1f) 
+                        Game.Score += 10;
+                        if (1-(Game.Score/5000.0f) < 0.1f) 
                         {
                             GridDisplay.SetTickTime(0.1f);
                         }else {
-                            GridDisplay.SetTickTime(1-(Game.score/5000.0f));
+                            GridDisplay.SetTickTime(1-(Game.Score/5000.0f));
                         }
                         return true;
                     }
@@ -134,7 +134,7 @@ public class Board{
         return false;
     }
     
-    // Cette fonction permet de vérifier si une pièce est touché
+
     public static void FloorTouch(){
         for (int i = 0;i<Game.MirrorGrid.Count;i++){
             for (int j =0;j<Game.MirrorGrid[0].Count;j++){
@@ -148,14 +148,16 @@ public class Board{
                 Game.MirrorGrid[i][j]=SquareColor.TRANSPARENT;
             }
         }
-        Board.SpawnPiece();
+        IsGameOver();
+        if (!Game.Gameover) {
+            Board.SpawnPiece();
+        }
     }
 
-    
     private static bool testSpawnBlock() {
-        for (int i = 0; i < 22; i++)
+        for (int i = 0; i < Game.Grid.Count; i++)
         {
-            for (int j = 0; j < 10; j++)
+            for (int j = 0; j < Game.Grid[0].Count; j++)
             {
               if (Game.MirrorGrid[i][j] != SquareColor.TRANSPARENT && Game.Grid[i][j] != SquareColor.TRANSPARENT) {
                 return true;
@@ -167,9 +169,10 @@ public class Board{
 
     // Cette fonction vérifie si une forme arrive en haut de la grille
     public static void IsGameOver(){
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < Game.Grid[0].Count; i++){
             if (Game.Grid[0][i]!= SquareColor.TRANSPARENT){
                 GameOver();
+                return ;
             }
         }
         
